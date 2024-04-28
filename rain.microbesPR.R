4da3be1e05f2291f61b27526a570720aceef222b
Mvviggi <mviggiano@battelleecology.org>
2024-04-28 17:11:38 -0400

    new branch pull from main

---
+++ rain.microbesPR.R
@@ -0,0 +1,135 @@
+##NEONScience: Time Series 03: Cleaning & Subsetting Time Series Data in R - NoData Values & Subset by Date
+#setup your working directory
+getwd() #this gives you where the working directory is currently
+setwd("C:/mviggiano/Documents/GitHub/Mvviggi")
+
+#Download packages
+install.packages("lubridate")
+install.packages("ggplot2")
+
+library(lubridate)
+library(ggplot2)
+
+#name the variables
+dpID<- "DP1.00006.001"
+
+#Use neonUtilities to pull data from Portal
+precip<- loadByProduct(dpID,site=c("CUPE", "GUIL"), 
+              startdate="2018-01", enddate="2023-01",
+              package= 'basic', check.size=F, release = 'current')
+list2env(precip, .GlobalEnv)
+
+str(SECPRE_30min)
+# Create new column with start time convertto to POSIX date time class - ATL time zone
+SECPRE_30min$datetime <- as.POSIXct(SECPRE_30min$startDateTime,
+                                    format = "%Y-%m-%dT%H:%M",
+                                    tz = "Atlantic Standard Time")
+#after this expression in line 24, realized the date/time is in local time zone.- ignore
+
+#convert startDateTime to Date variable
+SECPRE_30min$startDateTime<- as.Date(SECPRE_30min$startDateTime)
+class(CUPEprecip$startDateTime)
+str(SECPRE_30min)
+
+#renaming the stratDateTime to merge by this column with microbes data
+SECPRE_30min <-rename(SECPRE_30min,  date = startDateTime )
+
+##Calculate Daily Total Precipitation
+Dailyprecip<- SECPRE_30min %>%
+  group_by(siteID, date) %>%
+  summarise(dailyRain=sum(secPrecipBulk))
+
+#view Rows with NA values
+Dailyprecip[is.na(Dailyprecip$dailyRain),]
+
+#check for NA values
+sum(is.na(Dailyprecip))
+
+
+#remove rows with NA values from Dailyprecip df
+Dailyprecip<- na.omit(Dailyprecip, cols =c(dailyRain))
+
+#subset by siteID
+CUPEdaily<- Dailyprecip[Dailyprecip$siteID == "CUPE",]
+GUILdaily<-Dailyprecip[Dailyprecip$siteID == "GUIL",]
+str(CUPEprecip)
+
+
+# plot precip data for date range available
+CUPEplot<- qplot (date, dailyRain,
+       data= CUPEdaily,
+       main= "Bulk Precipitation at CUPE from 2018-2022",
+       xlab= "Date", ylab= "Daily Total Precipitation (mm)")+
+        scale_x_date(date_breaks = "1 month",
+                    date_labels = "%b-%Y")+
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+CUPEplot
+
+# Define Start and end times for the subset as R objects that are the time class
+startTime <- as.Date("2018-05-01")
+endTime <- as.Date("2023-01-01")
+
+# create a start and end time R object
+start.end <- c(startTime,endTime)
+start.end
+
+#joining tables into one single dataframe
+CUPERainMicrobesDF <- merge(CUPEdaily, CUPEmicrobe, by = "date", all.x = TRUE)
+GUILRainMicrobesDF <- merge(GUILdaily, GUILmicrobe, by = "date", all.x = TRUE)
+summary(RainMicrobesDF)
+
+#Joines dataframe with only available data
+filteredCUPERainMicrobe<- na.omit(CUPERainMicrobesDF)
+
+###plotting multiple visualizations
+
+CUPEmonthyears <-ggplot(data= CUPEdaily, aes(x=date, y= dailyRain))+
+  geom_line()+
+  scale_x_date(limits = start.end,
+               date_breaks= "1 month",
+               date_labels="%b-%Y"
+  ) +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))+
+  labs(title = "CUPE Precipitation")+
+  theme(plot.title = element_text(hjust =0.5))
+CUPEmonthyears
+
+####
+CUPEMxPplot<- filteredCUPERainMicrobe %>%
+  ggplot(aes(x=date, y = log10Abundance)) +
+  geom_line(color ="blue")+
+  geom_point(color ="blue")
+
+CUPEMxPplot + 
+  geom_line(aes(y= dailyRain), color = "red")+
+  geom_point()+
+  scale_y_continuous(breaks = seq(0,8,1),
+                     name="Microbial Abundance per mL",
+                     sec.axis = sec_axis( trans = ~. /2,
+                                         breaks = seq(0,60,10),
+                                         name = "Precipitation (mm)"
+                                         )
+                      ) +
+  theme(axis.title.y.left = element_text(color= "blue"),
+        axis.title.y.right = element_text(color= "red"))
+
+###correlation between daily precipitation and log10Abundance
+
+CUPEDailylogAb_corr<- filteredCUPERainMicrobe %>%
+  ggplot(aes(x= dailyRain, y= log10Abundance))+
+  geom_point()+
+  geom_smooth(method = "lm", se = FALSE, color = "red")+
+  facet_wrap(~Year, scale = "free")
+
+
+
+# microbesPrecip<- ggplot() +
+#   geom_line(data= CUPEprecip, aes(x=startDateTime, y= secPrecipBulk), color = "blue") +
+#   geom_line(data=CUPEmicrobe, aes(x=date, y= microAbMl/10), color = "green")+
+#   scale_x_date(date_breaks = "1 month",
+#                date_labels = "%b-%Y") +
+#   scale_y_continuous(name = "MicrobialAbundance per mL",
+#                      sec.axis = sec_axis(~./1000, name = "Precipitation (mm)"))+
+#   theme(axis.text.x = element_text(angle = 60, hjust = 1))
+# microbesPrecip
+
---
+++ waterqualit.microbeabundance.R
@@ -0,0 +1,197 @@
+##Nutrients and microbes
+
+getwd() #this gives you where the working directory is currently
+setwd("C:/mviggiano/Documents/GitHub/Mvviggi")
+#Script to calculate median nutrient by month/year for Aquatic sites. Plot sites monthly by year.
+install.packages("tidyverse")
+library(tidyverse)
+library(ggplot2)
+library(neonUtilities)
+library(tidyr)
+library(dplyr)
+install.packages("lubridate",repos="http://cran.us.r-project.org")
+library(lubridate)
+
+options(stringsAsFactors=F)
+data.swc<-loadByProduct(dpID="DP1.20093.001",
+                        site=c("CUPE", "GUIL"),
+                        startdate="2017-01", enddate="2023-06",
+                        package= 'basic', check.size=F, release = 'current')
+list2env(data.swc, .GlobalEnv)
+
+joinFieldswc<- joinTableNEON(swc_fieldSuperParent,
+                        swc_fieldData)
+
+str(swc_externalLabDataByAnalyte)
+head(joinFieldswc)
+#no need to join external lab and field tables
+
+swc.lab<-data.swc$swc_externalLabDataByAnalyte
+
+#reduce dataframe for swc.lab with needed columns only
+short.swc.lab<- swc.lab %>%
+  select(siteID, collectDate, analyte, analyteConcentration,sampleCondition)
+#checks
+str(short.swc.lab)
+
+#create colunm for date in as.Date
+short.swc.lab$date<-as.Date((short.swc.lab$collectDate), format ="%d-%b-%Y")
+
+#remove rows with analyte concentrtion as NA
+short.swc.lab<- short.swc.lab%>% drop_na(analyteConcentration)
+
+#convert Year column to factor
+short.swc.lab$Year <-format(short.swc.lab$date, "%Y")
+short.swc.lab$Month <-format(short.swc.lab$date, "%b")
+short.swc.lab$Year<- as.factor(short.swc.lab$Year)
+short.swc.lab$Month<- as.factor(short.swc.lab$Month)
+short.swc.lab <- short.swc.lab %>%
+  mutate(Month = factor(Month, levels =c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep","Oct", "Nov", "Dec")))
+
+#check for na values
+sum(is.na(short.swc.lab))
+summary(short.swc.lab)
+which(is.na(short.swc.lab$analyteConcentration))
+
+sum(is.na(ShortAbundanceDF))
+
+#joining water lab analytes and microbes into one single dataframe
+swc.microbes<- merge(ShortAbundanceDF,short.swc.lab,  by= "date", all.x = TRUE)
+
+##create column from analyteconcentration as log10
+swc.microbes<- swc.microbes %>%
+  mutate(log10analyte= log10(analyteConcentration))
+str(swc.microbes)
+##reduce columns and renamce columns
+swc.microbes<- swc.microbes %>%
+  select(date, siteID.x, microAbMl, Year.x, Month.x, log10Abundance, collectDate, analyte, sampleCondition, log10analyte)
+colnames(swc.microbes) <- c("Date", "siteID", "microAbMl", "Year", "Month", 
+                            "log10Abundance", "collectDate", "analyte", "sampleCondition", "log10analyte")
+sum(is.na(swc.microbes))
+#divide by siteID-- not yet
+
+tp.rows
+
+##dataframes by analyte
+swc.tn.<-short.swc.lab[short.swc.lab$analyte=='TN',]
+swc.toc<-short.swc.lab[short.swc.lab$analyte=='TOC',]
+swc.tp<-short.swc.lab[short.swc.lab$analyte=='TP',]
+swc.spc<-short.swc.lab[short.swc.lab$analyte=='specificConductance',]
+swc.plot<-short.swc.lab[short.swc.lab$analyte %in% c('TN','TP', 'specificConductance', 'TSS'),]
+swc.tss<-short.swc.lab[short.swc.lab$analyte=='TSS',]
+
+#tp.rows<-swc.plot$analyte=='TP' #rows with TP data
+#spc.rows<- swc.plot$analyte=='specificConductance'
+##dataframes by analyte and microbes
+swc.tn.mic<-swc.microbes[swc.microbes$analyte=='TN',]
+swc.toc.mic<-swc.microbes[swc.microbes$analyte=='TOC',]
+swc.tp.mic<-swc.microbes[swc.microbes$analyte=='TP',]
+swc.spc.mic<-swc.microbes[swc.microbes$analyte=='specificConductance',]
+swc.plot.mic<-swc.microbes[swc.microbes$analyte %in% c('TN','TP', 'specificConductance', 'TSS'),]
+swc.tss.mic<-swc.microbes[swc.microbes$analyte=='TSS',]
+
+tp.rows<-swc.plot$analyte=='TP' #rows with TP data
+spc.rows<- swc.plot$analyte=='specificConductance'
+
+summary(swc.tn)
+
+###
+##swc.plot$analyteConcentration[tp.rows]<-swc.plot$analyteConcentration[tp.rows]*10 #multiply TP by 10
+#swc.plot<-swc.lab[swc.lab$analyte %in% c('TN','TOC','TP'),]
+### ggplot nutrients only
+{
+  ggplot(data=swc.plot,aes(x=collectDate,y=analyteConcentration,color=analyte,group=analyte))+
+  geom_point()+
+  geom_line()+
+  facet_wrap(~siteID.x, nrow=2, scale= "free")
+str(swc.plot)
+ggplot(data=swc.plot,aes(x=collectDate,y=analyteConcentration,color=analyte,group=analyte))+
+  geom_point()+
+  geom_line()+
+  facet_wrap(~siteID, nrow=2, scale= "free")+
+  labs(y='TN and TOC (mg/L)',x='Collection Date',title='CUPE and GUIL Nutrients')+
+  scale_y_continuous(sec.axis = sec_axis(~.*5,name='TP (mg/L)'))+ #create a second y axis that is 10x
+  scale_x_date(date_labels = "%b-%Y", date_breaks = "1 year", date_minor_breaks = "1 month") 
+swc.plot$collectDate<- as.Date(swc.plot$collectDate)
+swc.plot<- na.omit(swc.plot)
+}
+
+
+#plotting microbes and TN
+{
+
+TNmicrobePlot<-swc.tn %>%
+  ggplot(aes(x= date, y=log10Abundance))+
+  geom_line(color="blue")+
+  scale_x_date(date_breaks= "1 month",
+               date_labels="%b-%Y") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1, size= 12),
+        axis.text.y = element_text(size= 12),
+        axis.title.x = element_text(size=12),
+        axis.title.y = element_text(size=12))+
+  labs(title = "Microbial abundance and TN  over time")+
+  theme(plot.title = element_text(hjust =0.5))+
+  facet_wrap(~siteID.x, ncol= 1, scale="free")
+  
+TNmicrobePlot +
+  geom_line(aes(y=analyteConcentration), color= "red") +
+  scale_y_continuous(breaks = seq(0,8,1),
+                     name = "log10 of Microbial Abundance per mL",
+                     sec.axis = sec_axis(trans = ~. *0.2 ,
+                                         breaks= seq(0,3,0.2),
+                                         name= "Total Nitrogen mg/L"
+                                         )
+                     )+
+  theme(axis.title.y.left = element_text(color= "blue"),
+        axis.title.y.right = element_text(color= "red"))
+
+}
+
+#plotting microbes and Tss dry mass in log10
+TssmicrobePlot<-swc.tss.mic %>%
+  ggplot(aes(x= Date, y=log10Abundance))+
+  geom_line(color="blue")+
+  scale_x_date(date_breaks= "1 month",
+               date_labels="%b-%Y") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1, size= 12),
+        axis.text.y = element_text(size= 12),
+        axis.title.x = element_text(size=12),
+        axis.title.y = element_text(size=12))+
+  labs(title = "Microbial abundance and TSS  over time")+
+  theme(plot.title = element_text(hjust =0.5))+
+  facet_wrap(~siteID.x, ncol= 1, scale="free")
+
+TssmicrobePlot +
+  geom_line(aes(y=log10analyte), color= "red") +
+  scale_y_continuous(#breaks = seq(0,8,1),
+                     name = "log10 of Microbial Abundance per mL",
+                     sec.axis = sec_axis(trans = ~./2,
+                                         #breaks= seq(0,9,1),
+                                         name= "Total Suspended Sediments mg/L"
+                     )
+  )+
+  theme(axis.title.y.left = element_text(color= "blue"),
+        axis.title.y.right = element_text(color= "red"))
+
+
+#linear regressions
+TNlinerPlot<- swc.tn.mic %>%
+  ggplot(aes(x=log10analyte, y= log10Abundance, color= Year, group = Year))+
+  geom_point()+
+  geom_smooth(method = "lm", se = FALSE, color = "red", formula = y ~ x)+
+  facet_wrap(~siteID, scale = "free")
+TNlinerPlot
+  
+
+TsslinerPlot<- swc.tss %>%
+  filter(analyteConcentration < c(20)) %>%
+  ggplot(aes(x= analyteConcentration, y=log10Abundance))+
+  geom_point()+
+  geom_smooth(method = "lm", se = FALSE, color = "red", formula = y ~ x)+
+  facet_wrap(~siteID, scale = "free")
+TsslinerPlot
+
+
+
+
+
